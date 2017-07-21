6aeeb6100e04e7480e91f89ea09d841b6c24e709
michaelkunc <mjkunc@gmail.com>
2017-07-21 13:53:19 -0700

    project structure and config

---
+++ instance/config.py
@@ -0,0 +1,39 @@
+import os
+
+
+class Config(object):
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    MONGO_DB = 'erpsy'
+    MONGO_URI = 'mongodb://localhost:27017/erpsy'
+
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Configurations for Testing."""
+    TESTING = True
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    DEBUG = True
+
+
+class StagingConfig(Config):
+    """Configurations for Staging."""
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
