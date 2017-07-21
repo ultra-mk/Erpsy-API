95e683611282e37a8821ab5a48e6a675499d1a5f
michaelkunc <mjkunc@gmail.com>
2017-07-21 14:02:03 -0700

    skeleton and connections

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask_api import FlaskAPI
+from flask_pymongo import PyMongo
+from instance.config import app_config
+
+
+def create_app(config_name):
+    app = FlaskAPI(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    mongo = PyMongo(app)
+
+    return app
