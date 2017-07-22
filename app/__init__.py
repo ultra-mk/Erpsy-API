8a92e790c2c672095e4b3af27834f38487e4e2a8
michaelkunc <mjkunc@gmail.com>
2017-07-21 21:33:43 -0700

    start of testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask_api import FlaskAPI
-from flask_pymongo import PyMongo
+from flask_pymongo import MongoClient
 from instance.config import app_config
 
 
@@ -7,6 +7,11 @@ def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    mongo = PyMongo(app)
+    mongo = MongoClient('localhost', 27017)
+    db = mongo['erpsy']
 
+    @app.route('/parts/', methods=['GET'])
+    def parts():
+        parts = db['parts'].find()
+        return [{'part_number': p['part_number'], 'name': p['part_name'], 'description':p['description']} for p in parts]
     return app
--- test_erpsy.py
+++ test_erpsy.py
@@ -1,8 +1,7 @@
 import unittest
-import os
-import json
 from app import create_app
 from flask_pymongo import MongoClient
+import json
 
 
 class ErpsyTest(unittest.TestCase):
@@ -10,20 +9,29 @@ class ErpsyTest(unittest.TestCase):
     def setUp(self):
         self.app = create_app(config_name='testing')
         self.client = self.app.test_client
-        self.part = {"part_name": "Polystyrene PWB",
+        self.part = {"name": "Polystyrene PWB",
                      "description": "DC Block Type Reference 22320f",
                      "part_number": "30758314"}
 
-        mongo = MongoClient('localhost', 27017)
-        db = mongo['test']
-        collection = db['parts']
+        self.mongo = MongoClient('localhost', 27017)
+        db = self.mongo['test']
+        self.collection = db['parts']
+        self.collection.insert_one(self.part)
 
-        collection.insert_one(self.part)
-
-    def test_stuff(self):
+    def test_parts_response(self):
         res = self.client().get('/parts/')
         self.assertEqual(res.status_code, 200)
 
+    def test_parts_format(self):
+        res = self.client().get('/parts/')
+        keys = set(json.loads(res.get_data(as_text=True))[
+            0].keys())
+        self.assertEqual(
+            keys, set(['part_number', 'description', 'name']))
+
+    def tearDown(self):
+        self.collection.delete_many({})
+        self.mongo.close()
 
 if __name__ == '__main__':
     unittest.main()
